779b8ffe6dd4eb87e010bafd9e912ff9ed853e47
Elliot Bulmer <egbulmer@icloud.com>
2023-12-07 20:30:20 +0800

    Day 2 - Part 2

--- src/day2.jl
+++ src/day2.jl
@@ -8,6 +8,31 @@ struct Game
 end
 
 """
+    minimum_set(game)
+
+Calculate the minimum required set of cubes required to make this game possible.
+"""
+function minimum_set(game::Game)
+    req_r, req_g, req_b = zeros(Int, 3)
+    for (r, g, b) in game.sets
+        req_r = max(req_r, r)
+        req_g = max(req_g, g)
+        req_b = max(req_b, b)
+    end
+    return (req_r, req_g, req_b)
+end
+
+"""
+    power_of(set)
+
+Calculate the power of a set as r * g * b.
+"""
+function power_of(set::Tuple{Int, Int, Int})
+    r, g, b = set
+    return r * g * b
+end
+
+"""
     parse_set(str)
 
 Parse a string of the form '3 blue, 4 red, 1 green' into a tuple of three integers: (r, g, b).
@@ -72,6 +97,23 @@ function sum_of_possible_games(filename)
         if is_game_possible(game, bag)
             sum += game.id
         end
+    end
+    return sum
+end
+
+"""
+    sum_of_minimum_set_powers(filename)
+
+Read file one line at a time, parse each line into a Game object and calculate the minimum 
+set for the game. Return the sum of the power of each minimum set.
+"""
+function sum_of_minimum_set_powers(filename)
+    sum = 0
+    for line in readlines(filename)
+        game = parse_game(line)
+        min_set = minimum_set(game)
+        power = power_of(min_set)
+        sum += power
     end
     return sum
 end
